dd9136cf3167d26d81fd79c43d580c728a3a9864
Manu-Almeida90 <emanuelle.almeida.batista@escola.pr.gov.br>
2026-08-06 08:49:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll("button");
 
 botoes.forEach(function (botao) {
     let curtiu = false;
-    botao.addEventListener("click", botaoClicado)
+    botao.addEventListener("click", botaoClicado);
     function botaoClicado() {
         console.log("fui clicado");
         let texto = botao.querySelector("span");
@@ -14,5 +14,16 @@ botoes.forEach(function (botao) {
             curtiu = false;
         }
     }
+})
+
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+btnTemaEscuro.addEventListener("click", mudaTema);
+
+function mudaTema() {
+    const corpoPagina = document.body;
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
 }
-)
